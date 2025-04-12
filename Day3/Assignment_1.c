1a6aa4d243a4cde69faf7210e6ff3d4bf2fa036d
Manu Krishna <krishnamanu27112004@gmail.com>
2025-04-12 13:57:36 +0530

    Create Assignment_1.c

---
+++ Day3/Assignment_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float mark;
+	printf("Enter the mark:");
+	scanf("%f", &mark);
+	if(mark > 50){
+		printf("Passed");
+	}else{
+		printf("Failed");
+	}
+	return 0;
+}
