3bbf2b7cff22fd0ad401b41f90f92aaceb7f2c4f
Manu Krishna <krishnamanu27112004@gmail.com>
2025-04-15 08:30:14 +0530

    Create A.java

---
+++ Java/A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class A {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num=s.nextInt();
+		if(num > 0) {
+			System.out.println("Number is positive.");
+		}else if(num <0) {
+			System.out.println("Number is negative");
+		}else {
+			System.out.println("Number is neither positive nor negetive, its zero.");
+		}
+		s.close();
+		}
+
+}
