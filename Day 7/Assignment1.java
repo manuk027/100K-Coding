87ff056d5bf52c8652d57fa40108c228b00a0ab7
Manu Krishna <krishnamanu27112004@gmail.com>
2025-04-18 11:48:56 +0530

    Create Assignment1.java

---
+++ Day 7/Assignment1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Prime {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		Number number=new Number();
+		System.out.print("Enter a number :");
+		number.num = s.nextInt();
+		for (int i=2; i<number.num/2; i++) {
+			if(number.num % i == 0) {
+				number.flag=0;
+				break;
+			}
+		}
+		if(number.flag == 1) {
+			System.out.println("The number is prime.");
+		}else {
+			System.out.println("The number is not prime");
+		}
+		
+	}
+
+}
+
+class Number {
+	int num;
+	int sum;
+	int flag=1;
+}
